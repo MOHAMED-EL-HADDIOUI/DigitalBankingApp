9230174388ce567d3733e171cb56234f89c15070
MOHAMED-EL-HADDIOUI <mohamedelhaddioui99@gmail.com>
2023-05-28 08:36:20 +0200

    First Commit

---
+++ BackEnd/src/main/java/com/elhaddioui/mohamed/digital/banking/dtos/CustomerDTO.java
@@ -0,0 +1,14 @@
+package com.elhaddioui.mohamed.digital.banking.dtos;
+
+import lombok.Data;
+
+
+@Data
+public class CustomerDTO {
+    private Long id;
+    private String name ;
+    private String email;
+//    private AppUser appUser;
+
+
+}
---
+++ BackEnd/src/main/java/com/elhaddioui/mohamed/digital/banking/security/services/AccountService.java
@@ -0,0 +1,16 @@
+package com.elhaddioui.mohamed.digital.banking.security.services;
+
+import com.elhaddioui.mohamed.digital.banking.security.entities.AppRole;
+import com.elhaddioui.mohamed.digital.banking.security.entities.AppUser;
+
+import java.util.List;
+
+public interface AccountService {
+    AppUser addNewUser(AppUser appUser);
+    AppRole addNewRole(AppRole appRole);
+    void addRoleToUser(String username, String rolename);
+    AppUser loadUserByUsername(String username);
+    List<AppUser> listUsers();
+
+    AppUser update(AppUser appUser);
+}
